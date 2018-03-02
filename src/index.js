2037be8494ae8a8d727f12a29ee7a026ecf6081e
edaineko <daineko.ekaterina@gmail.com>
2018-03-02 15:09:08 +0300

    zeros solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    
+    var count = 0;
+
+    while (number >= 5) {
+        number = Math.floor(number / 5);
+        count = count + number;
+    }
+
+    return count;
 }
+
